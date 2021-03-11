5e41fcf52f2d065bc48a1432c046d37e74814bb9
xwilliamxiao <80023759+xwilliamxiao@users.noreply.github.com>
2021-03-10 23:34:32 -0500

    added lose menu/scene

---
+++ Assets/Scripts/LoseMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseMenu : MonoBehaviour
+{
+    // Lose menu is going to be at scene 3 (so we gotta -3 to go back to the start/play menu
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
+    // This will quit the game after when we export/build it.
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
