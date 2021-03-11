6c930b7b67b4718a02a3564c6d91f78546711360
rachelleelcn <rachelleelcn@gmail.com>
2021-03-11 18:09:32 -0500

    fly movement update

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     
-    public float xy_speed;
-    public float z_speed;
+    public float x_force;
+    public float y_force;
+    public float z_force;
     public float rotateSpeed;
+
+    private Rigidbody rb;
+    private Vector3 angularSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
 
+        rb = GetComponent<Rigidbody>();
 
     }
 
@@ -20,38 +25,56 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(0, 0, 0);
+        Vector3 direction = new Vector3(0,0,0);
+        Quaternion rotation = Quaternion.Euler(angularSpeed * Time.deltaTime);
 
-       if (Input.GetKey(KeyCode.W))
-        {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f * xy_speed * Time.deltaTime, transform.position.z);
-            gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(-40, transform.rotation.y, transform.rotation.z), rotateSpeed * Time.deltaTime);
-        }
+        if (Input.GetKey(KeyCode.W))
+         {
+             direction = direction + Vector3.up;
+             gameObject.GetComponent<Rigidbody>().AddForce(direction * y_force * Time.deltaTime);
+         }
         if (Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f * xy_speed * Time.deltaTime, transform.position.z);
-            gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(40, transform.rotation.y, transform.rotation.z), rotateSpeed * Time.deltaTime);
+            direction = direction + Vector3.down;
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * y_force * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            direction = direction + Vector3.left * 0.01f * xy_speed * Time.deltaTime;
-            gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.x, -40, transform.rotation.z), rotateSpeed * Time.deltaTime);
-            
+            direction = direction + Vector3.left;
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * x_force * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            gameObject.transform.position = new Vector3(transform.position.x + 0.01f * xy_speed * Time.deltaTime, transform.position.y, transform.position.z);
-            gameObject.transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.rotation.x, 40, transform.rotation.z), rotateSpeed * Time.deltaTime);
+            direction = direction + Vector3.right;
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * x_force * Time.deltaTime);
         }
         if (Input.GetMouseButton(0))
         {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.01f * z_speed * Time.deltaTime);
+            direction = direction + Vector3.forward;
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * z_force * Time.deltaTime);
         }
         if (Input.GetMouseButton(1))
         {
-            gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.01f * z_speed * Time.deltaTime);
+            direction = direction + Vector3.back;
+            gameObject.GetComponent<Rigidbody>().AddForce(direction * z_force * Time.deltaTime);
         }
 
-        gameObject.transform.position += direction; //force
+        if (Input.GetKey(KeyCode.Q))
+        {
+            angularSpeed = new Vector3(0, -50, 0);
+            rb.MoveRotation(rb.rotation * rotation);
+
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            angularSpeed = new Vector3(0, 50, 0);
+            rb.MoveRotation(rb.rotation * rotation);
+
+        }
+
+
+
+        
+        
     }
 }
