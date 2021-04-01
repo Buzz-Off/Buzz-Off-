bdcf95a5de4b58685c0c66135df7f0379501b0f8
rachelleelcn <rachelleelcn@gmail.com>
2021-04-01 11:50:32 -0400

    enemyAnimation

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject fly;
+    public GameObject LArm;
+    public GameObject RArm;
+
+    public float lookDistance;
+    //public float attackDistance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackOpen();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector3.Distance(fly.transform.position, gameObject.transform.position) < lookDistance)
+        {
+            Vector3 direction = fly.transform.position - gameObject.transform.position;
+            direction.y = 0;
+            gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            
+        }
+        
+    }
+
+    private void attackOpen()
+    {
+        LArm.GetComponent<TwoBoneIKConstraint>().weight = 0;
+        RArm.GetComponent<TwoBoneIKConstraint>().weight = 0;
+    }
+    private void attackClose()
+    {
+        LArm.GetComponent<TwoBoneIKConstraint>().weight = 1;
+        RArm.GetComponent<TwoBoneIKConstraint>().weight = 1;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("enter");
+            attackClose();
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            Debug.Log("exit"); 
+            attackOpen();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/attackDetection.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class attackDetection : MonoBehaviour
+{
+
+    public GameObject fly;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == fly)
+        {
+            Debug.Log("lose");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
