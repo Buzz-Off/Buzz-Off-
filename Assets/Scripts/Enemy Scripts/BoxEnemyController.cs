0fb1d660c9332dabcd1ad3366911667896b962aa
rachelleelcn <rachelleelcn@gmail.com>
2021-04-13 20:26:14 -0400

    ad

--- Assets/Scripts/Enemy Scripts/BoxEnemyController.cs
+++ Assets/Scripts/Enemy Scripts/BoxEnemyController.cs
@@ -8,20 +8,34 @@ public class BoxEnemyController : MonoBehaviour
     public GameObject fly;
     public float speed;
     public float timer;
-
     private bool isChasing;
+
+    public Transform[] waypoints;
+    int currentWaypointID = 0;
+    NavMeshAgent navMeshAgent;
 
     // Start is called before the first frame update
     void Start()
     {
         isChasing = false;
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        navMeshAgent.SetDestination(waypoints[currentWaypointID].position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (navMeshAgent.remainingDistance < 0.04f)
+        {
+            currentWaypointID = (currentWaypointID + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[currentWaypointID].position);
+
+        }
+
         if (isChasing)
         {
+            gameObject.GetComponent<NavMeshAgent>().enabled = false;
+
             Vector3 direction = fly.transform.position - gameObject.transform.position;
             direction.y = 0;
 
@@ -33,6 +47,10 @@ public class BoxEnemyController : MonoBehaviour
                 gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, fly.transform.position, speed * Time.deltaTime);
 
             }
+        }
+        else
+        {
+            gameObject.GetComponent<NavMeshAgent>().enabled = true;
         }
     }
 
